16de0c0ef7c09bbde14bc5c7bed0fdc9c87cd2f0
Zhicheng Wang <zhichengwangS@outlook.com>
2019-07-13 23:47:40 +1000

    Choose, sight of Units, LayerManagement

---
+++ Assets/Scripts/BasicUnits.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BasicUnits : MonoBehaviour
+{
+    public float HP;
+    public float phAtk;
+    public float mgAtk;
+    public float phDef;
+    public float mgDef;
+    public float movSpd;
+    public float atSpd;
+    public float atDIs;
+    public float Exp;
+    public float pExp;
+    public float arExp;
+    public float sight = 8;
+    public int team = 1;
+    public bool atkMode;
+    public bool isChosen;
+    public bool isSeen = true;
+    public Console player;
+    public Rigidbody2D myBody;
+    GameObject follow;
+    GameObject tarEnemy;
+    GameObject areaEnemy;
+    GameObject atkMe;
+    Vector2 tarPoint;
+    int state = 0; // initial state is stop
+    Collider2D[] click;
+    List<GameObject> seenThings = new List<GameObject>();
+
+
+    void Start()
+    {
+        if (team != 1)
+        {
+            isSeen = false;
+        }
+        myBody.isKinematic = true;
+    }
+
+    GameObject ToAttack(GameObject tar)
+    {
+        state = 2; // attack tarEnemy
+        areaEnemy = null;
+        follow = null;
+        return tar.gameObject;
+    }
+    void Layer()
+    {
+        if (team != 1)
+        {
+            Collider2D[] round = Physics2D.OverlapCircleAll(transform.position, sight);
+            if (round.Length != 0)
+            {
+                foreach (Collider2D sth in round)
+                {
+                    float itsY = sth.transform.position.y;
+                    float myY = gameObject.transform.position.y;
+                    int myLayer = gameObject.GetComponent<Renderer>().sortingOrder;
+                    int itsLayer = sth.GetComponent<Renderer>().sortingOrder;
+                    if (myY > itsY && myLayer >= itsLayer)
+                    {
+                        gameObject.GetComponent<Renderer>().sortingOrder = itsLayer - 1;
+                    }
+                    if (myY < itsY && myLayer <= itsLayer)
+                    {
+                        gameObject.GetComponent<Renderer>().sortingOrder = itsLayer + 1;
+                    }
+
+                }
+            }
+        }
+    }
+
+    void IsSeen()
+    {
+        if(isSeen)
+        {
+            gameObject.GetComponent <SpriteRenderer>().enabled = true;
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+    void See()
+    {
+        if (team == 1)
+        {
+            Collider2D[] round = Physics2D.OverlapCircleAll(transform.position, sight); 
+            if (round.Length != 0)
+            {
+                foreach (Collider2D sth in round)
+                {
+                    float itsY = sth.transform.position.y;
+                    float myY = gameObject.transform.position.y;
+                    int myLayer = gameObject.GetComponent<Renderer>().sortingOrder;
+                    int itsLayer = sth.GetComponent<Renderer>().sortingOrder;
+                    if (myY > itsY && myLayer >= itsLayer)
+                    {
+                        gameObject.GetComponent<Renderer>().sortingOrder--;
+                    }
+                    if (myY < itsY && myLayer <= itsLayer)
+                    {
+                        gameObject.GetComponent<Renderer>().sortingOrder++;
+                    }
+                    if (sth.GetComponent<BasicUnits>() != null)
+                    {
+                        if (sth.GetComponent<BasicUnits>().team != 1 && !seenThings.Contains(sth.gameObject))
+                        {
+                            sth.GetComponent<BasicUnits>().isSeen = true;
+                            seenThings.Add(sth.gameObject);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    void DeleteSeen()
+    {
+        if (seenThings.Count != 0)
+        {
+            for (int i = 0; i < seenThings.Count; i++)
+            {
+                if (Vector3.Distance(transform.position, seenThings[i].transform.position) > sight)
+                {
+                    seenThings[i].GetComponent<BasicUnits>().isSeen = false;
+                    seenThings.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+    }
+
+    void State()
+    {
+        if(state == 1) 
+        {
+            transform.position = Vector2.MoveTowards(transform.position, tarPoint, movSpd * Time.deltaTime);
+        }
+        else if (state == 2)
+        {
+           /* if ()*/
+        }
+    }
+    
+    void LClick()
+    {
+        if(player.isModeAtk && Input.GetMouseButtonDown(0))
+        {
+            player.isModeAtk = false;
+            tarPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            click = Physics2D.OverlapCircleAll(tarPoint, 1);
+            if (click.Length != 0 && click[0].GetComponent<BasicUnits>() != null)
+            {
+
+                tarEnemy = ToAttack(click[0].gameObject);
+            }
+            else
+            {
+                state = 4; // attack at an point
+            }
+        }
+    }
+
+    void RClick()
+    {
+        if (Input.GetMouseButtonDown(1) && !player.isModeAtk)
+        {
+            tarPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            click = Physics2D.OverlapCircleAll(tarPoint, 1);
+            if (click.Length == 0 || click[0].GetComponent<BasicUnits>() == null)
+            {
+                state = 1; // receive ToMove order;
+                tarEnemy = null;
+                areaEnemy = null;
+                follow = null;
+            }
+            else
+            {
+                Collider2D tar = click[0];
+                if (tar.gameObject == gameObject)
+                {
+                    state = 0; // stop
+                }
+                else if (tar.GetComponent<BasicUnits>().team != team)
+                {
+                    tarEnemy = ToAttack(tar.gameObject);
+                }
+                else
+                {
+                    state = 3; // follow my units
+                    follow = tar.gameObject;
+                    tarEnemy = null;
+                    areaEnemy = null;
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        Layer();
+        IsSeen();
+        See();
+        DeleteSeen();
+    }
+}
---
+++ Assets/Scripts/Console.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Console : MonoBehaviour
+{
+    public int gold;
+    public int source;
+    public GameObject lineLeft;
+    public GameObject lineRight;
+    public GameObject lineDown;
+    public GameObject lineUp;
+    public List<GameObject> myUnits;
+    List<GameObject> chosen = new List<GameObject>();
+    List<GameObject> team1 = new List<GameObject>();
+    List<GameObject> team2 = new List<GameObject>();
+    List<GameObject> team3 = new List<GameObject>();
+    List<GameObject> team4 = new List<GameObject>();
+    List<GameObject> team5 = new List<GameObject>();
+    BasicUnits enemyChoice;
+    Vector2 box1;
+    Vector2 box1S;
+    public bool isModeAtk = false;
+    bool ishold;
+    bool isOrign = true;
+
+    private void Start()
+    {
+        lineLeft.GetComponent<SpriteRenderer>().enabled = false;
+        lineRight.GetComponent<SpriteRenderer>().enabled = false;
+        lineDown.GetComponent<SpriteRenderer>().enabled = false;
+        lineUp.GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    void Choose()
+    {
+        if(isOrign)
+        {
+            if(Input.GetMouseButtonDown(0) && !ishold)
+            {
+                ishold = true;
+                box1S = Input.mousePosition;
+                box1 = Camera.main.ScreenToWorldPoint(box1S);
+                
+            }
+            if (ishold)
+            {
+                lineLeft.GetComponent<SpriteRenderer>().enabled = true;
+                lineRight.GetComponent<SpriteRenderer>().enabled = true;
+                lineDown.GetComponent<SpriteRenderer>().enabled = true;
+                lineUp.GetComponent<SpriteRenderer>().enabled = true;
+                Vector2 boxedS = Input.mousePosition;
+                Vector2 boxd = Camera.main.ScreenToWorldPoint(boxedS);
+                float x1 = box1.x;
+                float x2 = boxd.x;
+                float y1 = box1.y;
+                float y2 = boxd.y;
+                Vector2 h1 = new Vector2(x1, (y1 + y2) / 2);
+                Vector2 h2 = new Vector2(x2, (y1 + y2) / 2);
+                Vector2 v1 = new Vector2((x1 + x2) / 2, y1);
+                Vector2 v2 = new Vector2((x1 + x2) / 2, y2);
+                lineLeft.transform.position = h1;
+                lineRight.transform.position = h2;
+                lineUp.transform.position = v1;
+                lineDown.transform.position = v2;
+                float scaleH = Mathf.Abs(box1S.x - boxedS.x)/2070f;
+                float scaleV = Mathf.Abs(box1S.y - boxedS.y)/1170f;
+                lineLeft.transform.localScale = new Vector2(1, scaleV);
+                lineRight.transform.localScale = new Vector2(1, scaleV);
+                lineUp.transform.localScale = new Vector2(scaleH, 1);
+                lineDown.transform.localScale = new Vector2(scaleH, 1);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                lineLeft.GetComponent<SpriteRenderer>().enabled = false;
+                lineRight.GetComponent<SpriteRenderer>().enabled = false;
+                lineDown.GetComponent<SpriteRenderer>().enabled = false;
+                lineUp.GetComponent<SpriteRenderer>().enabled = false;
+                Vector2 box2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                ishold = false;
+                Collider2D[] myunits = Physics2D.OverlapAreaAll(box1, box2);
+                List<GameObject> boxChoice = new List<GameObject>();
+                List<GameObject> enemyBox = new List<GameObject>();
+                if (myunits != null)
+                {
+                    foreach (Collider2D unit in myunits)
+                    {
+                        BasicUnits mine1 = unit.GetComponent<BasicUnits>();
+                        if (mine1 != null)
+                        {
+                            if (mine1.team == 1)
+                            {
+                                mine1.isChosen = true;
+                                boxChoice.Add(unit.gameObject);
+                            }
+                            else
+                            {
+                                enemyChoice = mine1; 
+                            }
+                        }
+                        if (boxChoice.Count > 0)
+                        {
+                            enemyChoice = null;
+                            if (chosen.Count != 0)
+                            {
+                                foreach (GameObject a in chosen)
+                                {
+                                    if (!boxChoice.Contains(a))
+                                    {
+                                        a.GetComponent<BasicUnits>().isChosen = false;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                chosen = boxChoice;
+                            }
+                        }
+                        else if (enemyChoice != null)
+                        {
+                            foreach (GameObject mine in chosen)
+                            {
+                                mine.GetComponent<BasicUnits>().isChosen = false;
+                            }
+                            chosen.Clear();
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+    void ModeAtk()
+    {
+        if (Input.GetKeyDown("a") && isOrign && chosen.Count != 0)
+        {
+            isModeAtk = true;
+            isOrign = false;
+        }
+    }
+    void ExeModeAtk()
+    {
+        if(isModeAtk)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                isModeAtk = false;
+                isOrign = true;
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
